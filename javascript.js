e56012d05ffd1c0e830ddafcbf8d241ad80e22aa
splossion <124319437+splossion@users.noreply.github.com>
2026-09-22 07:15:26 -0600

    add webpage

---
+++ javascript.js
@@ -0,0 +1,23 @@
+const container = document.getElementById("cards-container");
+
+async function loadCards() {
+    const response = await fetch("data.json");
+    const data = await response.json();
+
+    data.forEach(anime => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <a class="anime-card" href="${anime.link}">
+            <h2>${anime.title}</h2>
+            <img src="${anime.image}">
+            <p>${anime.quantity} cards</p>
+            </a>
+        `;
+
+        container.appendChild(card);
+    });
+} 
+
+loadCards();
